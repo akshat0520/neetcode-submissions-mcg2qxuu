3564236c6c25c150fed72c498a962194176f021e
Akshat Pandey <23ucc512@lnmiit.ac.in>
2026-07-30 19:32:16 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int len=heights.length;
+        int l=0, r=len-1;
+        int max=0;
+        while(l<r)
+        {
+            max=Math.max(max,(r-l)*Math.min(heights[l],heights[r]));
+            if(heights[l]<=heights[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return max;
+    }
+}
